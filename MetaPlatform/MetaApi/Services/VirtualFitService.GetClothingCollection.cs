f0b6b6901acf75be3ad81d5486f949b607fdfdab
SCOUT\v.dezhurnyuk <v.dezhurnyuk@scout-gps.ru>
2024-12-15 19:59:12 +0300

    +++++

--- MetaPlatform/MetaApi/Services/VirtualFitService.GetClothingCollection.cs
+++ MetaPlatform/MetaApi/Services/VirtualFitService.GetClothingCollection.cs
@@ -22,18 +22,42 @@ namespace MetaApi.Services
         private ClothingItem[] GetClothingItems(string host, FileType fileType)
         {
             var uploadsPath = Path.Combine(_env.WebRootPath, fileType.GetFolderName());
-            string[] files = Directory.GetFiles(uploadsPath);
-            var clothingItem = new ClothingItem[files.Length];
-            for (int i = 0; i < files.Length; i++)
+            if (Directory.Exists(uploadsPath))
             {
-                string fileName = Path.GetFileName(files[i]);
-                clothingItem[i] = new ClothingItem
+                var clothingItem = new List<ClothingItem>();
+                string[] files = Directory.GetFiles(uploadsPath);
+                foreach(string file in files)
                 {
-                    Link = GenerateFileUrl(fileName, fileType, host),
-                };
+                    string fileName = Path.GetFileName(file);
+                    bool endsWith_T = fileName.Contains("_t.") && fileName.Substring(0, fileName.LastIndexOf('.')).EndsWith("_t");
+                    if (endsWith_T)
+                    {
+                        clothingItem.Add(new ClothingItem
+                        {
+                            Link = GenerateFileUrl(fileName, fileType, host),
+                        });
+                    }
+                }
+
+                return clothingItem.ToArray();
+
+                /*var clothingItem = new ClothingItem[files.Length/2];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string fileName = Path.GetFileName(files[i]);
+                    bool endsWith_T = fileName.Contains("_t.") && fileName.Substring(0, fileName.LastIndexOf('.')).EndsWith("_t");
+                    if (endsWith_T)
+                    {
+                        clothingItem[i] = new ClothingItem
+                        {
+                            Link = GenerateFileUrl(fileName, fileType, host),
+                        };
+                    }
+                }
+                return clothingItem;*/
             }
 
-            return clothingItem;
+            return new ClothingItem[0];
         }
     }
 }
