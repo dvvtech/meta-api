d5c837381073f5bda142eb3d1fad6f77219c31ab
SCOUT\v.dezhurnyuk <v.dezhurnyuk@scout-gps.ru>
2024-12-16 08:56:44 +0300

    ++++

--- MetaPlatform/MetaApi/Services/VirtualFitService.ConvertImgFromIPhone.cs
+++ MetaPlatform/MetaApi/Services/VirtualFitService.ConvertImgFromIPhone.cs
@@ -6,48 +6,69 @@ namespace MetaApi.Services
 {
     public partial class VirtualFitService
     {
-        public void Convert()
+        /// <summary>
+        /// Исправляет ориентацию изображения на основе EXIF-метаданных.
+        /// </summary>
+        /// <param name="file">Входное изображение в формате IFormFile.</param>
+        /// <returns>Исправленное изображение в виде IFormFile или исходный файл, если EXIF Orientation отсутствует.</returns>
+        public static IFormFile FixImageOrientation(IFormFile file)
         {
-            FixImageOrientation(@"C:\\Users\\v.dezhurnyuk\\Pictures\\AI_Foto\man1_1.jpeg", "C:\\Users\\v.dezhurnyuk\\Pictures\\AI_Foto\\567.jpg");
-        }
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("Файл не должен быть пустым", nameof(file));
 
-        public static void FixImageOrientation(string inputPath, string outputPath)
-        {
-            using (var image = Image.Load(inputPath))
+            using var inputStream = file.OpenReadStream();
+
+            // Загружаем изображение из входного потока
+            using var image = Image.Load(inputStream);
+
+            // Попытка получить значение ориентации из EXIF
+            if (image.Metadata.ExifProfile == null ||
+                !image.Metadata.ExifProfile.TryGetValue(ExifTag.Orientation, out var orientationValue))
             {
-                // Попытка получить значение ориентации из EXIF
-                if (image.Metadata.ExifProfile != null &&
-                    image.Metadata.ExifProfile.TryGetValue(ExifTag.Orientation, out var orientationValue))
-                {
-                    ushort orientation = orientationValue.Value;
-
-                    switch (orientation)
-                    {
-                        case 3: // Поворот на 180 градусов
-                            image.Mutate(x => x.Rotate(RotateMode.Rotate180));
-                            break;
-
-                        case 6: // Поворот на 90 градусов по часовой стрелке
-                            image.Mutate(x => x.Rotate(RotateMode.Rotate90));
-                            break;
-
-                        case 8: // Поворот на 270 градусов против часовой стрелки
-                            image.Mutate(x => x.Rotate(RotateMode.Rotate270));
-                            break;
-                    }
-
-                    // Удаляем EXIF Orientation, чтобы предотвратить повторное применение
-                    image.Metadata.ExifProfile.RemoveValue(ExifTag.Orientation);
-
-                    // Сохраняем изображение
-                    image.Save(outputPath);
-                    Console.WriteLine("Ориентация исправлена и изображение сохранено.");
-                }
-                else
-                {
-                    Console.WriteLine("EXIF Orientation не найден. Изображение не изменено.");
-                }
+                // Если тега Orientation нет, возвращаем исходный файл
+                return file;
             }
+
+            ushort orientation = orientationValue.Value;
+            bool orientationFixed = false;
+
+            // Применяем поворот на основе значения ориентации
+            switch (orientation)
+            {
+                case 3: // Поворот на 180 градусов
+                    image.Mutate(x => x.Rotate(RotateMode.Rotate180));
+                    orientationFixed = true;
+                    break;
+                case 6: // Поворот на 90 градусов по часовой стрелке
+                    image.Mutate(x => x.Rotate(RotateMode.Rotate90));
+                    orientationFixed = true;
+                    break;
+                case 8: // Поворот на 270 градусов против часовой стрелки
+                    image.Mutate(x => x.Rotate(RotateMode.Rotate270));
+                    orientationFixed = true;
+                    break;
+            }
+
+            // Если ориентация не была исправлена, вернуть исходный файл
+            if (!orientationFixed)
+                return file;
+
+            // Удаляем EXIF Orientation, чтобы предотвратить повторное применение
+            image.Metadata.ExifProfile.RemoveValue(ExifTag.Orientation);
+
+            // Сохраняем исправленное изображение в MemoryStream
+            var outputStream = new MemoryStream();
+            image.SaveAsJpeg(outputStream); // Сохраняем в формате JPEG
+            outputStream.Position = 0; // Сброс позиции потока для чтения
+
+            // Создаём новый IFormFile из исправленного изображения
+            var fixedFile = new FormFile(outputStream, 0, outputStream.Length, file.Name, file.FileName)
+            {
+                Headers = file.Headers,
+                ContentType = "image/jpeg"
+            };
+
+            return fixedFile;
         }
     }
 }
