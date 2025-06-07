aeb91225db8fc7f687ce15b462751b7dca383e1f
DVV_COMPUTER\biker <dvv153m@gmail.com>
2025-06-07 18:56:23 +0300

    +++

--- MetaPlatform/MetaApi/Controllers/Auth/GoogleAuthorizeController.cs
+++ MetaPlatform/MetaApi/Controllers/Auth/GoogleAuthorizeController.cs
@@ -1,4 +1,5 @@
-﻿using MetaApi.Services.Auth;
+﻿using MetaApi.Models.Auth;
+using MetaApi.Services.Auth;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MetaApi.Controllers
@@ -26,35 +27,13 @@ namespace MetaApi.Controllers
 
         [HttpGet("callback")]
         public async Task<IActionResult> Callback(string code)
-        {            
-            var tokenResponse = await _authService.HandleCallback(code);
-
-            // Устанавливаем куки с токенами
-            /*Response.Cookies.Append("access_token", tokenResponse.AccessToken, new CookieOptions
-            {
-                HttpOnly = true, // Защита от XSS
-                Secure = true,    // Только HTTPS
-                SameSite = SameSiteMode.Strict, // Защита от CSRF
-                Expires = DateTime.UtcNow.AddHours(1) // Время жизни access-токена
-            });
-
-            Response.Cookies.Append("refresh_token", tokenResponse.RefreshToken, new CookieOptions
-            {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict,
-                Expires = DateTime.UtcNow.AddDays(7) // Время жизни refresh-токена
-            });
-
-            // Редирект на фронтенд
-            return Redirect("https://virtual-fit.one");*/
+        {
+            TokenResponse tokenResponse = await _authService.HandleCallback(code);
 
             //Перенаправляем пользователя на фронтенд
             return Redirect($"https://virtual-fit.one?" +
                             $"accessToken={Uri.EscapeDataString(tokenResponse.AccessToken)}&" +
-                            $"refreshToken={Uri.EscapeDataString(tokenResponse.RefreshToken)}");
-
-            //return Redirect($"https://localhost:7105/connect/?accessToken={tokenResponse.AccessToken}&refreshToken={tokenResponse.RefreshToken}");            
+                            $"refreshToken={Uri.EscapeDataString(tokenResponse.RefreshToken)}");                     
         }
     }
 }
