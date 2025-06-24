fc62664402cd61f42210004d02e87b342927460c
DVV_COMPUTER\biker <dvv153m@gmail.com>
2025-06-24 14:47:31 +0300

    add new item in history

--- MetaPlatform/MetaApi/Services/VirtualFitService.GetHistory.cs
+++ MetaPlatform/MetaApi/Services/VirtualFitService.GetHistory.cs
@@ -25,7 +25,11 @@ namespace MetaApi.Services
                     FittingHistory.Create(accountId: 1,
                                           garmentImgUrl: GenerateFileUrl("2e39b1d1-dfc0-4c41-a600-e5b72da6220a_1.435_t.png", FileType.Examples, host),
                                           humanImgUrl: GenerateFileUrl("547babf5-f046-4b73-aa2c-a7d4494298d3_1.481_t.png", FileType.Examples, host),
-                                          resultImgUrl: GenerateFileUrl("7abd958d-eb5b-4180-a64f-c988fc536f02_t.jpg", FileType.Examples, host))
+                                          resultImgUrl: GenerateFileUrl("7abd958d-eb5b-4180-a64f-c988fc536f02_t.jpg", FileType.Examples, host)),
+                    FittingHistory.Create(accountId: 1,
+                                          garmentImgUrl: GenerateFileUrl("de419a4d-d232-40a6-b8ca-d065788d1c4c_1.593_t.png", FileType.Examples, host),
+                                          humanImgUrl: GenerateFileUrl("3ac9d1c3-4fae-455b-9bef-30e9a2395a7b_2.433_t.png", FileType.Examples, host),
+                                          resultImgUrl: GenerateFileUrl("9b9ad00a-47b9-48e8-b960-b73fffc6eabe_t.jpg", FileType.Examples, host))
             };
 
             
